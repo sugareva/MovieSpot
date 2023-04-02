bdcc1e25484b4b49ab1be2be629df1fd86d97e5b
Sugareva <43824066+sugareva@users.noreply.github.com>
2023-04-02 14:41:24 +0200

    flex fixed

---
+++ img/Guess the Movie_fichiers/movies.js
@@ -0,0 +1,99 @@
+ const movies = [
+    {
+      name: ["Friends", "Friend"],
+      image: "./img/img12.jpg"
+    },
+    {
+        name: ["Breaking Bad", "Breakings Bad"],
+        image: "./img/img2.jpg"
+      },
+      {
+        name: ["Parasite", "Parasites"],
+        image: "./img/img5.jpg"
+      },
+      {
+        name: ["The Office", "Office"],
+        image: "./img/img17.jpg"
+      },
+      {
+        name: ["Shining", "The Shining", "Shinings"],
+        image: "./img/img9.jpg"
+      },
+      {
+        name: ["Better Call Saul", "Call Saul", "Better Call"],
+        image: "./img/img18.jpg"
+      },
+      {
+        name: ["American Horror Story", "AHS"],
+        image: "./img/img23.jpg"
+      },
+      {
+        name: ["Alien", "Aliens"],
+        image: "./img/img15.jpg"
+      },
+      {
+        name: ["Desperate Housewives", "Desesperate Housewives", "Desperate Housewive", "Desperates Housewive", "Desperates Housewives"],
+        image: "./img/img8.jpg"
+      },
+      {
+        name: ["The Haunting of Hill House", "The Haunting Of", "Haunting Of", "Haunting Of Hill House"],
+        image: "./img/img22.jpg"
+      },
+      {
+        name: ["Stranger Things", "Stranger Thing"],
+        image: "./img/img16.jpg"
+      },
+      {
+        name: ["The Boys", "The Boy"],
+        image: "./img/img20.png"
+      },
+      {
+        name: ["Dexter"],
+        image: "./img/img1.jpg"
+      },
+      {
+        name: ["The Sopranos", "Sopranos", "The Soprano"],
+        image: "./img/img3.png"
+      },
+      {
+        name: ["Shameless"],
+        image: "./img/img21.jpg"
+      },
+      {
+        name: ["Parks and Recreation", "Parks and Rec", "Park and Rec", "Park and Recreation", "Park and Recreations"],
+        image: "./img/img14.jpg"
+      },
+      {
+        name: ["Mad Men", "Mad Mens"],
+        image: "./img/img4.jpg"
+      },
+      {
+        name: ["Tenet"],
+        image: "./img/img6.jpg"
+      },
+      {
+        name: ["The Big Lebowski", "Big Lebowski"],
+        image: "./img/img7.jpg"
+      },
+      {
+        name: ["The Walking Dead", "Walking Dead"],
+        image: "./img/img19.jpg"
+      },
+      {
+        name: ["American Psycho"],
+        image: "./img/img10.jpg"
+      },
+      {
+        name: ["Monk"],
+        image: "./img/img13.jpg"
+      },
+      {
+        name: ["Pulp Fiction"],
+        image: "./img/img3.jpg"
+      },
+      {
+        name: ["Batman Begins", "The Dark Knight", "Dark Knight", "Batman"],
+        image: "./img/img11.jpg"
+      },
+     
+  ];
---
+++ img/Guess the Movie_fichiers/script.js
@@ -0,0 +1,207 @@
+
+let stage = document.getElementById('stage');
+let currentIndex = 0;
+let img = document.getElementById("img");
+const nextButton = document.querySelector("#next-button");
+const input = document.querySelector("#userInput");
+let toastIcon = document.getElementById("toast");
+const infoIndex = document.getElementById("textIndex");
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+function showSeries(index) {
+  if (currentIndex === 24){
+    const gameCard = document.getElementById("gameCard");
+    const successCard = document.getElementById("successCard");
+
+    stage.textContent = `Current score : ${currentIndex} / ${movies.length}`;
+    document.getElementById("myProgress").value = currentIndex; 
+    gameCard.classList.add("is-hidden");
+    successCard.classList.remove("is-hidden");
+    return;
+  }
+  //Change image according to the index
+  img.src = movies[index].image;
+  //Change score
+  stage.textContent = `Current score : ${currentIndex} / ${movies.length}`;
+  //Update progress bar
+  document.getElementById("myProgress").value = currentIndex; 
+
+}
+
+showSeries(currentIndex);
+
+//if the user press enter, or the button, launch checkSeries
+input.addEventListener("keydown", function(event) {
+    if (event.code === "Enter") {
+      checkSeries();
+    }
+});
+
+nextButton.addEventListener("click", checkSeries);
+
+//Takes user's input and check if it's the good one and update the index OR restart
+function checkSeries() {
+  const userInput = input.value.toLowerCase();
+  const possibleNames = movies[currentIndex].name;
+  //if good answer, continue the game
+  for (let i = 0; i < possibleNames.length; i++) {
+    if (userInput === possibleNames[i].toLowerCase()) {
+    successToast();
+    document.getElementById("message").textContent = yesses[getRandomInt(0, yesses.length - 1)];
+    currentIndex++;
+    input.value = "";
+    showSeries(currentIndex);
+    return;   
+    }
+  } 
+  // if there's no correspondance
+  errorToast();
+  document.getElementById("message").textContent = `Your final score is ${currentIndex}. Try again!` 
+  currentIndex = 0;
+  input.value = "";
+  showSeries(currentIndex);
+}
+
+//Progress Bar animation
+
+let i = 0;
+ function move() {
+    if (currentIndex == 0) {
+      i = 1;
+      var elem = document.getElementById("myBar");
+      var width = 1;
+      var id = setInterval(frame, 10);
+      function frame() {
+        if (width >= 100) {
+          clearInterval(id);
+          i = 0;
+        } else {
+          width++;
+          elem.style.width = width + "%";
+        }
+      }
+    }
+  } 
+
+
+//array of random success feedback
+var yesses = [
+  "Yes! Yes! Yes!",
+  "Indeed",
+  "Correct!",
+  "Well Done!",
+  "Ole!",
+  "Detective skills : 100%",
+  "You REALLY spent too much time on Netflix",
+  "Perfecto",
+  "Excellent!",
+  "Wonderful!",
+  "Fabulous!",
+  "Hooray!",
+  "Top Notch",
+  "Are you cheating?",
+  "Woohoo!",
+  "Amazing!",
+  "Superstar!",
+  "You're good!",
+  "You got it!",
+  "Great Job!",
+  "Good!",
+  "Great!",
+  "Totally",
+]
+
+//Success and error toasts
+function successToast() {
+  // Get the snackbar DIV
+  toast.src = "https://img.icons8.com/3d-fluency/94/null/best-seller.png";
+  var x = document.getElementById("snackbar");
+  
+  // Add the "show" class to DIV
+  x.className = "show";
+    
+  // After 3 seconds, remove the show class from DIV
+  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+} 
+
+function errorToast() {
+  // Get the snackbar DIV
+  toast.src = "https://img.icons8.com/3d-fluency/94/null/disappointed.png";
+  var x = document.getElementById("snackbar");
+
+  // Add the "show" class to DIV
+  x.className = "show";
+    
+  // After 3 seconds, remove the show class from DIV
+  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+} 
+
+function refreshPage(){
+  window.location.reload();
+} 
+
+// Modal icon hover animation
+
+function hover(element) {
+  element.src = "https://img.icons8.com/ios-glyphs/30/ffc700/info-squared.png";
+}
+  
+function unhover(element) {
+  element.src = 'https://img.icons8.com/ios-glyphs/30/null/info-squared.png';
+}
+
+
+//MODAL BOX
+
+infoIndex.textContent = `You have ${movies.length} pieces to find and each mistake makes you start again from the beginning. Several spellings are accepted,
+and the game does not take into account capital letters.`;
+
+
+  document.addEventListener('DOMContentLoaded', () => {
+    // Functions to open and close a modal
+    function openModal($el) {
+      $el.classList.add('is-active');
+    }
+  
+    function closeModal($el) {
+      $el.classList.remove('is-active');
+    }
+  
+    function closeAllModals() {
+      (document.querySelectorAll('.modal') || []).forEach(($modal) => {
+        closeModal($modal);
+      });
+    }
+  
+    // Add a click event on buttons to open a specific modal
+    (document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
+      const modal = $trigger.dataset.target;
+      const $target = document.getElementById(modal);
+  
+      $trigger.addEventListener('click', () => {
+        openModal($target);
+      });
+    });
+  
+    // Add a click event on various child elements to close the parent modal
+    (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
+      const $target = $close.closest('.modal');
+  
+      $close.addEventListener('click', () => {
+        closeModal($target);
+      });
+    });
+  
+    // Add a keyboard event to close all modals
+    document.addEventListener('keydown', (event) => {
+      const e = event || window.event;
+  
+      if (e.keyCode === 27) { // Escape key
+        closeAllModals();
+      }
+    });
+  });
